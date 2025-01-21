911035f3ecad53212b407a27b167e9d7c570ba3d
Your Name <memberty.45@gmail.com>
2025-01-22 06:03:58 +0800

    finished my courses

--- src/components/Educator/MyCourse.js
+++ src/components/Educator/MyCourse.js
@@ -39,7 +39,8 @@ function MyCourse() {
     setIsConfirmationModalOpen(true); 
   };
 
-  const handleDeleteClick = () => {
+  const handleDeleteClick = (courseName) => {
+    setCourseName(courseName);
     setConfirmationMessage('Are you sure you want to delete?');
     setConfirmationAction('delete');
 
@@ -260,7 +261,7 @@ function MyCourse() {
                 viewBox="0 -960 960 960"
                 width="24px"
                 fill="#000000"
-                onClick={handleDeleteClick}
+                onClick={() => handleDeleteClick(course.course_name)}
                 className="cursor-pointer"
               >
                 <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/>
