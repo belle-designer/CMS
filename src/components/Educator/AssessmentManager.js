69811e884ee60ce8b32ce81f900a2c9a740d98f9
Your Name <memberty.45@gmail.com>
2025-01-22 10:15:24 +0800

    removed other buttons

--- src/components/Educator/AssessmentManager.js
+++ src/components/Educator/AssessmentManager.js
@@ -164,31 +164,6 @@ function AssessmentManager() {
             <td className="px-4 py-2">Variables, Loops, Lists</td>
             <td className="px-4 py-2">In Progress</td>
             <td className="px-4 py-2 flex items-center justify-center space-x-4">
-              {/* {}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-                fill="#000000"
-                onClick={handleSaveClick}
-                className="cursor-pointer"
-              >
-                <path d="M840-680v480q0 33-23.5 56.5T760-120H200q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h480l160 160Zm-80 34L646-760H200v560h560v-446ZM480-240q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35ZM240-560h360v-160H240v160Zm-40-86v446-560 114Z"/>
-              </svg>
-
-              {}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-                fill="#000000"
-                onClick={() => handleEditClick("Python Basics", "Variables, Loops, Lists", "Learn the basics of Python programming.", "Understand syntax, loops, and lists.", "N/A")}
-                className="cursor-pointer"
-              >
-                <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h357l-80 80H200v560h560v-278l80-80v358q0 33-23.5 56.5T760-120H200Zm280-360ZM360-360v-170l367-367q12-12 27-18t30-6q16 0 30.5 6t26.5 18l56 57q11 12 17 26.5t6 29.5q0 15-5.5 29.5T897-728L530-360H360Zm481-424-56-56 56 56ZM440-440h56l232-232-28-28-29-28-231 231v57Zm260-260-29-28 29 28 28 28-28-28Z"/>
-              </svg> */}
 
               {}
               <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000" onClick={handleDeleteClick} className="cursor-pointer">
